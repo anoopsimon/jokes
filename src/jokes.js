67c9561267a45625ffba029f5d30a83583f9124e
Anoop Simon <s451421990@gmail.com>
2021-10-13 17:25:46 +1100

    added more jokes display

--- src/jokes.js
+++ src/jokes.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { createBreakpoints } from "@chakra-ui/theme-tools";
-import { Link, Box, Text, Image ,Center, Square, Circle} from "@chakra-ui/react";
+import {
+  Link,
+  Box,
+  Text,
+  Image,
+  Center,
+  Fade,
+  ScaleFade,
+  Slide,
+  SlideFade,
+  Heading,
+  HStack,
+  Tag,
+  CircularProgress
+} from "@chakra-ui/react";
 
 const breakpoints = createBreakpoints({
   sm: "30em",
@@ -15,69 +29,87 @@ class Jokes extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: null,
+      jokes: [],
+      loading:true,
+      images: ["cat.jpg", "cat1.jpg", "cat2.jpg"],
     };
   }
-   componentDidMount = async()=> {
+
+  componentDidMount = async () => {
     const headers = {
       "Content-Type": "text/plain",
     };
-    await axios.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit&type=single").then(
-      (res) => {
-        console.log("Response : " + res.data);
-        this.setState({ jokes: res.data });
-      },
-      { headers }
-    );
-  }
+    await axios
+      .get(
+        "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single&amount=10"
+      )
+      .then(
+        (res) => {
+          console.log("Response : " + res.data);
+          this.setState({ jokes: res.data.jokes ,loading:false});
+        },
+        { headers }
+      );
+  };
 
   render() {
+    if(this.state.loading){
+      return <Center>
+        <CircularProgress isIndeterminate color="green.300">
+        </CircularProgress>
+        </Center>
+    }
     return (
       <>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Center  h="100px" color="white">
+        <br></br>
         <Box p={4} display={{ md: "flex" }}>
-          <Box flexShrink={0}>
-            <Image
-              borderRadius="lg"
-              width={{ md: 40 }}
-              src="https://bit.ly/2jYM25F"
-              alt="Woman paying for a purchase"
-            />
-          </Box>
-          <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
-            <Text
-              fontWeight="bold"
-              textTransform="uppercase"
-              fontSize="sm"
-              letterSpacing="wide"
-              color="teal.600"
-            >
-              {this.state.jokes?.category}
-            </Text>
-            <Link
-              mt={1}
-              display="block"
-              fontSize="lg"
-              lineHeight="normal"
-              fontWeight="semibold"
-              href="#"
-            >
-              {this.state.type}
-            </Link>
-            <Text mt={2} color="gray.500">
-              {this.state.jokes?.joke}
-            </Text>
-          </Box>
+          <Center>
+          <Heading
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip="text"
+          fontSize="6xl"
+          fontWeight="extrabold"
+          >Joke.
+          </Heading>
+          </Center>
         </Box>
+        <Center>
+          <Box p={4} display={{ md: "flex" }}>
+            {this.state.jokes.map((joke, key) => (
+              <Box key={key}
+               border={3}
+               borderRadius={"lg"} 
+               marginBottom="2rem"
+               width={"fit-content"}
+               blockSize={"fit-content"}
+               
+               >
+               
+                <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+                  <Text
+                    fontWeight="bold"
+                    textTransform="uppercase"
+                    fontSize="sm"
+                    letterSpacing="wide"
+                    color="teal.600"
+                  >
+                    {joke.category}
+                  </Text>
+
+                  <Text
+                    mt={2}
+                    color="gray.500"
+                    fontSize="lg"
+                    bgGradient="linear(to-l, #7928CA, #FF0080)"
+                    bgClip="text"
+                    fontWeight="extrabold"
+                  >
+                    {joke.joke}
+                  </Text>
+                </Box>
+              </Box>
+            ))}
+          </Box>
         </Center>
       </>
     );
