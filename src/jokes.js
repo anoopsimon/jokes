b747649ee7d9b65cf0d60a42866e5ed3ab34a5f0
Anoop Simon <s451421990@gmail.com>
2021-10-13 17:38:24 +1100

    updated styling

--- src/jokes.js
+++ src/jokes.js
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
 import axios from "axios";
-import {
-  Box,
-  Text,
-  Center,
-  Heading,
-  CircularProgress
-} from "@chakra-ui/react";
-
-
+import { Box, Text, Center, Heading, CircularProgress } from "@chakra-ui/react";
 
 class Jokes extends Component {
   constructor(props) {
     super(props);
     this.state = {
       jokes: [],
-      loading:true
+      loading: true,
     };
   }
 
@@ -29,72 +21,86 @@ class Jokes extends Component {
       )
       .then(
         (res) => {
-          this.setState({ jokes: res.data.jokes ,loading:false});
+          this.setState({ jokes: res.data.jokes, loading: false });
         },
         { headers }
       );
   };
 
+  jokes = () => {
+    return this.state.jokes.map((joke, key) => (
+      <Box
+        key={key}
+        border={3}
+        borderRadius={"lg"}
+        bg="var(--chakra-colors-teal-300)"
+        marginBottom="2rem"
+        width={"fit-content"}
+        blockSize={"fit-content"}
+      >
+        <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
+          <Text
+            fontWeight="bold"
+            textTransform="uppercase"
+            fontSize="sm"
+            letterSpacing="wide"
+            color="teal.600"
+            paddingLeft=".6rem"
+            paddingTop=".6rem"
+
+          >
+            {joke.category}
+          </Text>
+
+          <Text
+            mt={2}
+            fontSize="lg"
+            bgClip="text"
+            fontWeight="bold"
+            color="white"
+            padding="1rem"
+          >
+            {joke.joke}
+          </Text>
+        </Box>
+      </Box>
+    ));
+  };
+
   render() {
-    if(this.state.loading){
-      return <Center>
-        <CircularProgress isIndeterminate color="#7928CA">
-        </CircularProgress>
+    if (this.state.loading) {
+      return (
+        <Center>
+          <CircularProgress isIndeterminate color="#7928CA"></CircularProgress>
         </Center>
+      );
     }
     return (
       <>
-        <br></br>
-        <Box p={4} display={{ md: "flex" }}>
-          <Center>
-          <Heading
-          bgGradient="linear(to-l, #7928CA, #FF0080)"
-          bgClip="text"
-          fontSize="6xl"
-          fontWeight="extrabold"
-          >Joke.
-          </Heading>
-          </Center>
-        </Box>
+        {this.header()}
         <Center>
           <Box p={4} display={{ md: "flex" }}>
-            {this.state.jokes.map((joke, key) => (
-              <Box key={key}
-               border={3}
-               borderRadius={"lg"} 
-               marginBottom="2rem"
-               width={"fit-content"}
-               blockSize={"fit-content"}
-               
-               >
-               
-                <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
-                  <Text
-                    fontWeight="bold"
-                    textTransform="uppercase"
-                    fontSize="sm"
-                    letterSpacing="wide"
-                    color="teal.600"
-                  >
-                    {joke.category}
-                  </Text>
-
-                  <Text
-                    mt={2}
-                    color="gray.500"
-                    fontSize="lg"
-                    bgGradient="linear(to-l, #7928CA, #FF0080)"
-                    bgClip="text"
-                    fontWeight="bold"
-                  >
-                    {joke.joke}
-                  </Text>
-                </Box>
-              </Box>
-            ))}
+            {this.jokes()}
           </Box>
         </Center>
       </>
+    );
+  }
+
+  header() {
+    return (
+      <Box p={4} display={{ md: "flex" }}>
+        <Center>
+          <Heading
+            bgGradient="linear(to-l, #7928CA, #FF0080)"
+            bgClip="text"
+            fontSize="6xl"
+            fontWeight="extrabold"
+          >
+            Joke.
+          </Heading>
+        </Center>
+      </Box>
     );
   }
 }
